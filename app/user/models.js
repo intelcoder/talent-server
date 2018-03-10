c233bbd814433547bb4b4e18bbf9b572bf710555
intelcoder <seongjunkim7162@gmail.com>
2018-03-10 15:22:21 -0500

    user added

--- app/user/models.js
+++ app/user/models.js
@@ -1,2 +1,34 @@
 const Sequelize = require('sequelize');
 
+const User = sequalize.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  lastName:{
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  fullName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  password: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  isActive: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+  lastLogin: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+  },
+  isAnonymous: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  } 
+})
